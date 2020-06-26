a7b8668f931625d1779c77c159c4759ea75b50a7
1BBC <bbc21249@gmail.com>
2020-06-26 14:08:57 +0300

    qwe

--- modules/admin/controllers/CacheController.php
+++ modules/admin/controllers/CacheController.php
@@ -8,7 +8,29 @@ class CacheController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $movieCount = Yii::$app->db->createCommand('SELECT MAX(id) FROM movie')
+            ->queryScalar();
+
+        $movieCacheCount = 0;
+
+        for ($i = $movieCount; $i >= 1; $i--) {
+            if (Yii::$app->cache->exists('movie' . $i) == 1) {
+                $movieCacheCount++;
+            }
+        }
+
+        $tvCount = Yii::$app->db->createCommand('SELECT MAX(id) FROM tv')
+            ->queryScalar();
+
+        $tvCacheCount = 0;
+
+        for ($i = $tvCount; $i >= 1; $i--) {
+            if (Yii::$app->cache->exists('tv' . $i) == 1) {
+                $tvCacheCount++;
+            }
+        }
+
+        return $this->render('index', ['tv' => $tvCacheCount, 'movie' => $movieCacheCount]);
     }
 
     /**
@@ -35,10 +57,51 @@ class CacheController extends \yii\web\Controller
         return $this->redirect(['index']);
     }
 
+    public function actionMovies()
+    {
+        $count = Yii::$app->db->createCommand('SELECT MAX(id) FROM movie')
+            ->queryScalar();
+
+        $delCount =0;
+
+        for ($i = $count; $i >= 1; $i--) {
+            if (Yii::$app->cache->delete('movie' . $i) == 1) {
+                $delCount++;
+            }
+        }
+
+        Yii::$app->session->setFlash('flash', "movies: удалено " . $delCount . " фильма");
+        return $this->redirect(['index']);
+    }
+
+    public function actionTvs()
+    {
+        $count = Yii::$app->db->createCommand('SELECT MAX(id) FROM tv')
+            ->queryScalar();
+
+        $delCount =0;
+
+        for ($i = $count; $i >= 1; $i--) {
+            if (Yii::$app->cache->delete('tv' . $i) == 1) {
+                $delCount++;
+            }
+        }
+
+        Yii::$app->session->setFlash('flash', "tvs: удалено " . $delCount . " сериала");
+        return $this->redirect(['index']);
+    }
+
     public function actionNetworks()
     {
         Yii::$app->cache->delete('networks');
         Yii::$app->session->setFlash('flash', "networks: Телесети обновлены");
+        return $this->redirect(['index']);
+    }
+
+    public function actionSitemap($id)
+    {
+        Yii::$app->cache->delete('sitemap' . $id);
+        Yii::$app->session->setFlash('flash', "Sitemap " . $id  . ": xml обновлен");
         return $this->redirect(['index']);
     }
 
