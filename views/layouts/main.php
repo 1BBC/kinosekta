b9e840d324647e5fb7b56b71bea4d364fe67143f
1BBC <bbc21249@gmail.com>
2020-07-17 16:53:01 +0300

    google analutics

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,6 +17,16 @@ SiteAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-172900719-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-172900719-1');
+    </script>
+
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
